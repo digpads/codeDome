68fdb5eea86ea399bf516c14f1f06a27ffb59ccc
digpads <digpads@gmail.com>
2020-03-22 12:02:54 +0800

    a

--- app.js
+++ app.js
@@ -6,33 +6,33 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
-app.use(enforceHttps());
+app.use(async ctx => {
+    console.log('hi https');
+  });
+
+http.createServer(app.callback()).listen(8081);
 
 const privateKey = fs.readFileSync(path.join(__dirname, './3639621.key'), 'utf8')
 const certificate = fs.readFileSync(path.join(__dirname, './3639621.pem'), 'utf8')
-
 
 const options = {
     key:privateKey,
     cert:certificate
 }
+https.createServer(options,app.callback()).listen(443);
 
-http.createServer(app.callback()).listen(8081);
 
-app.use(async ctx => {
-  console.log('http');
-});
-// 创建https服务器实例
-const httpsServer = https.createServer(options, async (req, res) => {
-    res.writeHead(200)
-    res.end('Hello https')
-  })
+// // 创建https服务器实例
+// const httpsServer = https.createServer(options, async (req, res) => {
+//     res.writeHead(200)
+//     res.end('Hello https')
+//   })
   
-  // 设置https的访问端口号
-  const SSLPORT = 8082
+//   // 设置https的访问端口号
+//   const SSLPORT = 8082
   
-  // 启动服务器，监听对应的端口
-  httpsServer.listen(SSLPORT, () => {
-    console.log(`HTTPS Server is running on: https://localhost:${SSLPORT}`)
-  })
+//   // 启动服务器，监听对应的端口
+//   httpsServer.listen(SSLPORT, () => {
+//     console.log(`HTTPS Server is running on: https://localhost:${SSLPORT}`)
+//   })
   
