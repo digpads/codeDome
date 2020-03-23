fc0df0e40f59cca9ba83ef886e625aab942a252e
digpads <digpads@gmail.com>
2020-03-23 15:34:06 +0800

    uphttps

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const Koa = require('koa');
+const route = require("koa-route");
+const static = require('koa-static');
+
 const enforceHttps = require('koa-sslify').default;
 const app = new Koa();
 const http = require('http');
@@ -7,10 +10,18 @@ const fs = require('fs');
 const path = require('path');
 
 app.use(enforceHttps());
- 
-app.use(function * (next) {
-    this.body = "hello world from " + this.request.url;
-});
+
+app.use(static(path.join(__dirname)+'/public/'));
+const routes = fs.readdirSync(path.resolve(__dirname,"./routes"));
+
+routes.forEach(v => {
+    const url = v.replace(/\.js$/i,"");
+    app.use(route.get(`/${url=="index"?"":url}`,require(`./routes/${v}`)))
+})
+
+
+app.use(route.get("*",require("./routes/error.js")))
+
 
 
 const privateKey = fs.readFileSync(path.join(__dirname, './3639621.key'), 'utf8')
